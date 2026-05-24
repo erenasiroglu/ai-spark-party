d8d8d1ca92e40c58681bfdfd892583fdae979e47
Eren <erennasiroglu@192.168.1.8>
2026-05-24 14:33:30 +0300

    fix: deploy errors

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-DdLWfOzB.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/erennasiroglu/Desktop/ai-spark-party/src/routes/__root.tsx", children: ["/", "/sitemap.xml"], preloads: ["/assets/index-T90W5FzF.js"] }, "/": { filePath: "/Users/erennasiroglu/Desktop/ai-spark-party/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-Yu7d_AT5.js"] } }, clientEntry: "/assets/index-T90W5FzF.js" });
-export {
-  tsrStartManifest
-};
